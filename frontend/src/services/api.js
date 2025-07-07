bf8b14fd3ebcb07834901819db8bf282d4b1c7f8
Raíza Cirne <raizacirne@gmail.com>
2025-07-07 16:33:20 -0300

    feat: add api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://user-management-system-zvcw.onrender.com",
 });
 
 export default api;
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3000",
+// });
+
+// export default api;
